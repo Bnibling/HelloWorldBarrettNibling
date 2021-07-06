678beca00a7e2d182c23619c064b39c93eae6e8a
Bnibling <bnibling@gmail.com>
2021-07-06 13:08:50 -0500

    Added 'num' argument to hello() function

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,6 @@
 class HelloWorld:
     def __init__(self):
         pass
-    def hello(self):
-        return "hello world!"
+    def hello(self, num):
+        for _ in range(num):
+            return "hello world!"
